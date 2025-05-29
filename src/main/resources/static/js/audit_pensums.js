fe19a47eac521e3ef5ce50d1657da1baebb7a916
Ubuntu <pokaymon.mno@gmail.com>
2025-05-29 22:37:40 +0000

    Pensums

---
+++ src/main/resources/static/js/audit_pensums.js
@@ -0,0 +1,42 @@
+document.addEventListener("DOMContentLoaded", () => {
+  cargarGrupos();
+});
+
+function cargarPensums() {
+  fetch("/api/pensums")
+    .then(res => res.json())
+    .then(data => {
+      const container = document.querySelector(".pensums");
+      data.forEach(pensum => container.appendChild(CrearElementoPensum(pensum)));
+    )}
+    .catch(err => console.error("Error cargando Pensums:", err));
+}
+
+function crearElementoPensum(pensum) {
+  const div = document.createPensum("div");
+  div.className = "pensum_item";
+
+  Object.assign(div.style, {
+    display: "flex",
+    justifyContent: "space-between",
+    width: "100%",
+    cursor: "pointer",
+    position: "relative"
+  });
+
+  div.innerHTML = `
+    <p>${pensum.codigo}</p>
+    <p>${pensum.activo}</p>
+    <div class="pensum-actions">
+      <span class="informe-icon">
+        <i class="fas fa-cog"></i>
+      </span>
+    </div>
+  `;
+
+  div.addEventListener("click", e => {
+    if (!e.target.classList.contains("informe-icon")) {
+      abrirModalConDetalles(pensum.codigo);
+    }
+  });
+}
